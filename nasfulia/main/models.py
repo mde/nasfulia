10f6b2c8a6b59e7e43e75abff485678a54bbd7ee
mde <mde@10515858-a55c-0410-baf4-8f8af48b4918>
2008-11-27 05:53:03 +0000

    Initial import
    git-svn-id: http://shiva.fleegix.org/svn/nasfulia/trunk@1 10515858-a55c-0410-baf4-8f8af48b4918

---
+++ nasfulia/main/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Notice(models.Model):
+    user = models.ForeignKey(User)
+    content = models.TextField()
+    reply_to_id = models.IntegerField()
+    created_at = models.DateTimeField()
+
+class Account(models.Model):
+    user = models.ForeignKey(User)
+    service_id = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    crypted_password = models.CharField(max_length=255)
+    enabled = models.BooleanField(default=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+class Track(models.Model):
+    user = models.ForeignKey(User)
+    service_id = models.CharField(max_length=255)
+    text = models.TextField()
+    enabled = models.BooleanField(default=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+
