46c3a5518809925d336ae3ec158b98cd338faaf3
mde <mde@10515858-a55c-0410-baf4-8f8af48b4918>
2008-12-04 07:11:46 +0000

    Redirect to home page now -- if user needs to create accounts, it will be detected by the emtpy array of accounts passed to the init sequence.
    git-svn-id: http://shiva.fleegix.org/svn/nasfulia/trunk@12 10515858-a55c-0410-baf4-8f8af48b4918

--- nasfulia/main/views/Main.py
+++ nasfulia/main/views/Main.py
@@ -6,20 +6,13 @@ from django.contrib.auth.models import User
 from nasfulia.main.models import Account
 
 def home(request):
-    return render_to_response('home.html', {})
-
-def profile(request):
     id = request.session.get('_auth_user_id')
     user = User.objects.get(id=id)
     accounts = Account.objects.filter(user__id=id)
     request.session['accounts'] = accounts
-    # User has service accounts set up, send them
-    # into the app
-    if len(accounts) > 0:
-        return render_to_response('home.html', {})
-    # Otherwise they need to set up some accounts
-    else:
-        return HttpResponseRedirect('/users/' +
-            user.username + '/accounts')
+    return render_to_response('home.html', {'accounts': accounts})
+
+def profile(request):
+    return HttpResponseRedirect('/home')
 
 
