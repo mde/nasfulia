319d0b2d096c8f1964e71626aca51912071827d0
mde <mde@10515858-a55c-0410-baf4-8f8af48b4918>
2008-12-28 06:43:33 +0000

    Make the password encrypt/decrypt into static methods on the Account object.
    git-svn-id: http://shiva.fleegix.org/svn/nasfulia/trunk@31 10515858-a55c-0410-baf4-8f8af48b4918

--- nasfulia/main/models.py
+++ nasfulia/main/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from random import randrange
+from Crypto.Cipher import Blowfish
+from base64 import b64encode, b64decode
 
 class Notice(models.Model):
     user = models.ForeignKey(User)
@@ -8,6 +11,24 @@ class Notice(models.Model):
     created_at = models.DateTimeField()
 
 class Account(models.Model):
+    @staticmethod
+    def encrypt_password(username, password_text):
+        c = Blowfish.new(username)
+        return b64encode(c.encrypt(Account.__pad(password_text)))
+
+    @staticmethod
+    def decrypt_password(username, crypted_text):
+        c = Blowfish.new(username)
+        return Account.__depad(c.decrypt(b64decode(crypted_text)))
+
+    @staticmethod
+    def __pad(input):
+        return input + "".join(["\n" for i in xrange(8 - len(input) % 8)])
+
+    @staticmethod
+    def __depad(input):
+        return input.rstrip("\n")
+
     user = models.ForeignKey(User)
     service_id = models.CharField(max_length=255)
     username = models.CharField(max_length=255)
